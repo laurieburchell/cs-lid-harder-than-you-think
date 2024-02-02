3ce9a741c04bf38a20f39cf69b814501a37cbe75
Laurie Burchell <laurie.burchell@ed.ac.uk>
2024-02-02 07:50:27 +0000

    begin cleaning script

---
+++ data/scripts/clean-data.py
@@ -0,0 +1,47 @@
+"""Cleans test sets in format 'sent \t labels...', outputting cleaned sentences to stdout
+
+author: laurie
+"""
+
+import argparse
+import sys
+import unicodedata
+from sacremoses import MosesPunctNormalizer
+
+from tools.remove_non_printing_char import get_replacer as non_printing_char_replacer
+from tools.demojizier import Demojizer
+
+class SentenceClean:
+    def __init__(self):
+        self.mpn = MosesPunctNormalizer(lang='en')
+        self.replace_nonprint = non_printing_char_replacer(" ")
+        self.demojiser = Demojizer()
+
+    def __call__(self, line):
+        clean = self.mpn.normalize(line)
+        clean = self.replace_nonprint(clean)
+        clean = unicodedata.normalize("NFKC", clean)
+        clean = self.demojiser(clean, "")
+
+        # remove twitter effects
+
+        return clean
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("in_file", help="test file in tab-separated format 'sent label1 label2")
+args = parser.parse_args()
+
+sent_cleaner = SentenceClean()
+
+with open(args.in_file) as f:
+    for line in f.readlines():
+        raw_sent = line.split('\t')[0].strip()
+        clean_sent = sent_cleaner(raw_sent)
+        print(clean_sent)
+        break
+
+
+
+
