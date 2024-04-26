2c6fc27874bc3a6973dd264cb48f5957751282b6
Laurie Burchell <laurie.burchell@ed.ac.uk>
2024-04-26 12:27:15 +0100

    add instructions to download models

---
+++ models/download-models.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eo pipefail
+
+# author: laurie
+# downloads models
+HOME_DIR=`pwd`
+
+# openlid - from https://github.com/laurieburchell/open-lid-dataset
+if ! [ -f lid201-model.bin.gz ]; then
+    echo "downloading OpenLID model"
+    wget https://data.statmt.org/lid/lid201-model.bin.gz
+else
+    echo "found lid201-model.bin.gz, skipping OpenLID model download"
+fi
+pigz -d lid201-model.bin.gz
+
+# multilid - download from University of Edinburgh's server
+if ! [ -f multilid-model.pt ]; then
+    echo "downloading MultiLID model"
+    wget https://data.statmt.org/lid/multilid-model.pt
+else
+    echo "found multilid-model.pt, skipping MultiLID model download"
+fi
+
+echo "we use pyfranc to access the franc-all model"
